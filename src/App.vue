f57d7d2df9f166ba63ebc507a73b590318c6e4c9
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-14 15:10:12 +0300

    use mappers

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,24 @@
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>
-        Счетчик {{ $store.getters.counter }} ({{
-          $store.getters.doubleCounter
-        }})
-      </h2>
+      <h2>Счетчик {{ counter }} ({{ doubleCounter }})</h2>
       <button class="btn" @click="add">Добавить</button>
-      <button class="btn danger" @click="incrementAsync">Добавить 10</button>
+      <button class="btn danger" @click="incAsync">Добавить 10</button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters, mapMutations } from "vuex";
 import TheNavbar from "./TheNavbar";
 export default {
   components: { TheNavbar },
+  computed: {
+    ...mapGetters(["counter", "doubleCounter"]),
+  },
   methods: {
-    add() {
-      this.$store.commit("increment");
-    },
-    doubleCounter() {
-      this.$store.commit("doubleCounter");
-    },
-    incrementAsync() {
-      this.$store.dispatch("incAsync");
-    },
+    ...mapMutations({ add: "increment" }),
+    ...mapActions(["incAsync"]),
   },
 };
 </script>
--- src/TheNavbar.vue
+++ src/TheNavbar.vue
@@ -1,20 +1,25 @@
 <template>
   <header class="navbar">
     Счетчик {{ counter }}
-    <button class="btn primary" @click="add">Добавить 5</button>
+    <button class="btn primary" @click="addFive">Добавить 5</button>
   </header>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.counter;
-    },
+    // counter() {
+    //   return this.$store.getters.counter;
+    // },
+    ...mapGetters(["counter"]),
   },
   methods: {
-    add() {
-      this.$store.commit("add", 5);
+    ...mapMutations(["add"]),
+    addFive() {
+      this.add(5);
+      // this.$store.commit("add", 5);
     },
   },
 };
